0103e29527119fa7bfd9b5918af7330c7652045b
Natalija Prosic <162211449+redsparrow98@users.noreply.github.com>
2024-03-17 16:14:24 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -29,29 +29,27 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 
 while game_is_on:
-  #use update at the start of each loop to see whats happened on screen
-  screen.update()
-  #slow down the time after each loop starts bu a little
-  time.sleep(0.1)
-  snake.move()
+	#use update at the start of each loop to see whats happened on screen
+	screen.update()
+	#slow down the time after each loop starts bu a little
+	time.sleep(0.1)
+	snake.move()
 
-  
-  #detect colision with food
-  if snake.head.distance(food) < 15:
-    food.refresh()
-    snake.extend()
-    scoreboard.increase_score()
+	#detect colision with food
+	if snake.head.distance(food) < 15:
+		food.refresh()
+		snake.extend()
+		scoreboard.increase_score()
 
-  #detect colision with wall
-  if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-    scoreboard.reset()
-    snake.reset()
+	#detect colision with wall
+	if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+		scoreboard.reset()
+		snake.reset()
 
-  #detecting colision with tale
-  for segment in snake.segments[1:]:
-    if snake.head.distance(segment) < 10:
-      scoreboard.reset()
-      snake.reset()
-      
+	#detecting colision with tale
+	for segment in snake.segments[1:]:
+		if snake.head.distance(segment) < 10:
+			scoreboard.reset()
+			snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -5,33 +5,33 @@ FONT =  ("Courier", 20, "normal")
 
 class Scoreboard(Turtle):
 
-    def __init__(self):
-      """Creates a scoreboard on the top middle of the screen, opens the data text file and reads the high score"""
-      super().__init__()
-      self.score = 0
-      with open("data.txt", mode= 'r') as data:
-        self.highscore = int(data.read())
-      self.penup()
-      self.hideturtle()
-      self.color("white")
-      self.goto(0, 265)
-      self.update_scoreboard()
+	def __init__(self):
+		"""Creates a scoreboard on the top middle of the screen, opens the data text file and reads the high score"""
+		super().__init__()
+		self.score = 0
+		with open("data.txt", mode= 'r') as data:
+			self.highscore = int(data.read())
+		self.penup()
+		self.hideturtle()
+		self.color("white")
+		self.goto(0, 265)
+		self.update_scoreboard()
 
-    def update_scoreboard(self):
-      """updates the score board adn writes it out"""
-      self.clear()
-      self.write(arg= f"Score: {self.score} High score: {self.highscore}", align= ALIGNMENT, font= FONT)
-  
-    def increase_score(self):
-      """clears the screen every time a score is updated and updates the score by 1"""
-      self.score += 1
-      self.update_scoreboard()
+	def update_scoreboard(self):
+		"""updates the score board adn writes it out"""
+		self.clear()
+		self.write(arg= f"Score: {self.score} High score: {self.highscore}", align= ALIGNMENT, font= FONT)
 
-    def reset(self):
-      """if the curent scor is bigger than previous high score then it updates it to the new one. also opens the data txt file and updates it there"""
-      if self.score > self.highscore:
-        self.highscore = self.score
-        with open("data.txt", mode= 'w') as data:
-          data.write(f"{self.highscore}")
-      self.score = 0
-      self.update_scoreboard()
+	def increase_score(self):
+		"""clears the screen every time a score is updated and updates the score by 1"""
+		self.score += 1
+		self.update_scoreboard()
+
+	def reset(self):
+		"""if the curent scor is bigger than previous high score then it updates it to the new one. also opens the data txt file and updates it there"""
+		if self.score > self.highscore:
+			self.highscore = self.score
+		with open("data.txt", mode= 'w') as data:
+			data.write(f"{self.highscore}")
+		self.score = 0
+		self.update_scoreboard()
