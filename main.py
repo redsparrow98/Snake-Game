8157c0019d683fa49b956e1d0d56e12604dea83c
Natalija Prosic <162211449+redsparrow98@users.noreply.github.com>
2024-03-21 15:05:03 +0000

    Add files via upload
    updated the code formatting to be in alignment with PEP 8

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-#the tracker turns off the animations and we use update to show whats hapened on screen
+# the tracker turns off the animations and we use update to show whats happened on screen
 screen.tracer(0)
 ##################################################
 
-#make the initial 3square snake object
+# make the initial 3square snake object
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
@@ -29,27 +29,27 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 
 while game_is_on:
-	#use update at the start of each loop to see whats happened on screen
-	screen.update()
-	#slow down the time after each loop starts bu a little
-	time.sleep(0.1)
-	snake.move()
+    # use update at the start of each loop to see whats happened on screen
+    screen.update()
+    # slow down the time after each loop starts bu a little
+    time.sleep(0.1)
+    snake.move()
 
-	#detect colision with food
-	if snake.head.distance(food) < 15:
-		food.refresh()
-		snake.extend()
-		scoreboard.increase_score()
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
-	#detect colision with wall
-	if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-		scoreboard.reset()
-		snake.reset()
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
 
-	#detecting colision with tale
-	for segment in snake.segments[1:]:
-		if snake.head.distance(segment) < 10:
-			scoreboard.reset()
-			snake.reset()
+    # detecting collision with tale
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -7,70 +7,70 @@ LEFT = 180
 RIGHT = 0
 
 class Snake:
-	"""Creates our 3 segment snake and puts it on screen next to each other it has,
-	square shape, white color, puts pen up"""
-	def __init__(self):
-		self.segments = []
-		self.create_snake()
-		self.head = self.segments[0]
+    """Creates our 3 segment snake and puts it on screen next to each other it has,
+    square shape, white color, puts pen up"""
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
-	def create_snake(self):
-		"""Create the original 3 squares on the starting positions next to each other"""
-		for position in STARTING_POSITIONS:
-			self.add_segment(position)
+    def create_snake(self):
+        """Create the original 3 squares on the starting positions next to each other"""
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
 
-	def add_segment(self, position):
-		new_segment = Turtle("square")
-		new_segment.color("white")
-		new_segment.penup()
-		new_segment.goto(position)
-		self.segments.append(new_segment)
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
-	def extend(self):
-		"""add a new segment to the snake"""
-		self.add_segment(self.segments[-1].position())
+    def extend(self):
+        """add a new segment to the snake"""
+        self.add_segment(self.segments[-1].position())
 
-	def reset(self):
-		"""sends the segments of the current snake in the list off screen then clears the segments list of previous items, then creates a new snake on the middle of the screen and sets the first segment as the head of the snake"""
-		for seg in self.segments:
-			seg.goto(1000, 1000)
-		self.segments.clear()
-		self.create_snake()
-		self.head = self.segments[0]
+    def reset(self):
+        """sends the segments of the current snake in the list off screen then clears the segments list of previous items, then creates a new snake on the middle of the screen and sets the first segment as the head of the snake"""
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
-	def move(self):
-		"""moves the snake forward continuosly and make the rest of the body follow the head"""
-		#we move last segment to second to last make a loop for it. the range is
-		#start = lenght of segments list, stop is 0 and not included that is 1st seg in our seg list.
-		#and step is -1 we are moving backwards in range
-		for seg_num in range(len(self.segments) -1, 0, -1):
-		#new x nad y is the position of the second to last in range and we
-		#get its x and y cordinates
-			new_x = self.segments[seg_num -1].xcor()
-			new_y = self.segments[seg_num -1].ycor()
-		#we want the last segment in the list to move to the position of the previous one.
-		#so we find the position of the previous one first
-		self.segments[seg_num].goto(x= new_x, y= new_y)
-		#and we move the head of the snake seg 0 and all the other ones follow
-		#to its previous position down the chaine
-		self.head.forward(MOVE_DISTANCE)
+    def move(self):
+        """moves the snake forward continuously and make the rest of the body follow the head"""
+        #we move last segment to second to last make a loop for it. the range is
+        #start = length of segments list, stop is 0 and not included that is 1st seg in our seg list.
+        #and step is -1 we are moving backwards in range
+        for seg_num in range(len(self.segments) -1, 0, -1):
+        #new x nad y is the position of the second to last in range and we
+        #get its x and y coordinates
+            new_x = self.segments[seg_num -1].xcor()
+            new_y = self.segments[seg_num -1].ycor()
+        #we want the last segment in the list to move to the position of the previous one.
+        #so we find the position of the previous one first
+        self.segments[seg_num].goto(x= new_x, y= new_y)
+        #and we move the head of the snake seg 0 and all the other ones follow
+        #to its previous position down the chain
+        self.head.forward(MOVE_DISTANCE)
 
-	def up(self):
-		"""turns snake head (segment[0]) and turns it to 90 degree direct"""
-		if self.head.heading() != DOWN:
-			self.head.setheading(UP)
+    def up(self):
+        """turns snake head (segment[0]) and turns it to 90 degree direct"""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
-	def down(self):
-		"""turns snake head (segment[0]) and turns it to 270 degree direct"""
-		if self.head.heading() != UP:
-			self.head.setheading(DOWN)
+    def down(self):
+        """turns snake head (segment[0]) and turns it to 270 degree direct"""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
-	def left(self):
-		"""turns snake head (segment[0]) and turns it to 180 degree direct"""
-		if self.head.heading() != RIGHT:
-			self.head.setheading(LEFT)
+    def left(self):
+        """turns snake head (segment[0]) and turns it to 180 degree direct"""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
-	def right(self):
-		"""turns snake head (segment[0]) and turns it to 0 degree direct"""
-		if self.head.heading() != LEFT:
-			self.head.setheading(RIGHT)
+    def right(self):
+        """turns snake head (segment[0]) and turns it to 0 degree direct"""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
