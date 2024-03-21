1c3c0f15c21d816ebc43702290eefea63b6a6eb2
Natalija Prosic <prosicnatalija98@gmail.com>
2024-03-21 20:15:18 +0000

    PEP 8 update

--- food.py
+++ food.py
@@ -2,7 +2,8 @@ from turtle import Turtle
 import random
 
 class Food(Turtle):
-    
+    """This class inherits from turtle module and it creates the food in the game"""
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -13,6 +14,7 @@ class Food(Turtle):
         self.refresh()
 
     def refresh(self):
+        """This function refreshes the food square at a random point on the screen"""
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
